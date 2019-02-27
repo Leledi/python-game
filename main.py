94a1975593f27e19adad5452c29b3ef01e754b41
studente <studente@liceospalla.local>
2019-02-27 09:30:26 +0100

    creato player e mostro

--- main.py
+++ main.py
@@ -1,8 +1,68 @@
 class Entity:
-  def __init__(self, x, y, graphic):
+  def __init__y, graphic(self, x, y, graphic):
     self.x = x
     self.y = y
     self.graphic = graphic
+
+class Mobile(Entity):
+	def __init__(self, x, y, graphic, maxHp, damage, level):
+		Entity.__init__(self, x, y, graphic)
+		self.maxHp=maxHp
+		self.curHp=maxHp
+		self.damage=damage
+		self.level=level
+
+	def attack(self,target):
+		target.curHp-=self.damage
+
+class Player(Mobile):
+	def __init__(self, x, y, graphic, maxHp, damage, level):
+		Entity.__init__(self, x, y, graphic, maxHp, damage, level)
+
+	def move(self, direction):
+		def move(self, direction):
+		if direction=="w":
+			if self.y==0:
+				pass
+			else:
+				self.y-=1
+
+		elif direction=="s":
+			if self.y==World1.h-1:
+				pass
+			else:
+				self.y+=1
+
+		elif direction=="d":
+			if self.x==World1.w-1:
+				pass
+			else:
+				self.x+=1
+
+		elif direction=="a":
+			if self.x==0:
+				pass
+			else:
+				self.x-=1
+
+class Enemy(Mobile):			
+	def __init__(self, x, y, graphic, maxHp, damage, level):
+		Entity.__init__(self, x, y, graphic, maxHp, damage, level)
+	def move(self):
+		direc=randint(0,4)
+		if direc==0:
+			if self.y!=0:
+				self.y-=1
+		elif direc==1:
+			if self.y!=World1.h-1:
+				self.y+=1
+		elif direc==2:
+			if self.x!=World1.w-1:
+				self.x+=1
+
+		elif direc==3:
+			if self.x!=0:
+				self.x-=1
 
 
 e = Entity(5, 5, "X")
